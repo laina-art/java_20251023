8131faf15671dd4fffa498261ed6949f0a019f49
laina-art <laina2207@gmail.com>
2025-10-24 12:25:56 +0900

    java

---
+++ Java20251023/src/ex01/If조건문08.java
@@ -0,0 +1,21 @@
+package ex01;
+
+public class If조건문08 {
+
+	public static void main(String[] args) {
+		
+		//double num = Math.random(); 
+		
+		/* 
+		 * 0 ~ 1 :  난수값 추출
+		 * 1 ~ 100값 출력
+		 */
+		
+		int num = (int)(Math.random() * 100);
+		System.out.println(num);
+		
+		System.out.println("------프로그램 종료--------");
+
+	}
+
+}
